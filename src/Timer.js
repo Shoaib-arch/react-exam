9ee0c81dd10d09c761bf14533cbc1345972a0068
ShoaibDev266 <shoaibahmedgtk5@gmail.com>
2023-05-27 17:25:48 +0500

    Creating the CountDown UseState

---
+++ src/Timer.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+//Creating the Timer function as arrow functions to take countdown state
+const Timer = () => {
+  const [countdown, setCountdown] = useState(10);
+  let timer;
+
+  
+ 
+
+
+};
+
+export default Timer;
